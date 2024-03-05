8a7a6cb56786b1a3bf01b06b43e727c9c8a0ff67
sagar <150423510+Sagar0546@users.noreply.github.com>
2024-03-05 16:47:06 +0530

    Update data.sh

--- data.sh
+++ data.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 sudo yum -y update
 sudo yum -y install git
-git clone https://github.com/03Aziz02/Medical-Insurance-py.git
+git clone https://github.com/Sagar0546/agri.git
 cd Medical-Insurance-py
 pip3 install -r requirements.txt
 screen -m -d python3 app.py
