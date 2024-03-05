718f1a61f57f86da5d5f63bb7aeeed0c13659cb2
sagar <150423510+Sagar0546@users.noreply.github.com>
2024-03-05 16:05:26 +0530

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -1,8 +1,8 @@
 # Creating EC2 Instance
 resource "aws_instance" "python" {
-  ami                         = "ami-013898da85dead62b"
+  ami                         = "ami-0eb4694aa6f249c52"
   instance_type               = "t2.micro"
-  key_name                    = "MK1"
+  key_name                    = "aws"
   vpc_security_group_ids      = [aws_security_group.sg.id]
   subnet_id                   = aws_subnet.subnet-1.id
   associate_public_ip_address = true
