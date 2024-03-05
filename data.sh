9f323dbe5b9a59cfef0f50ed03226d1e006d22f0
sagar <150423510+Sagar0546@users.noreply.github.com>
2024-03-05 16:52:24 +0530

    Update data.sh

--- data.sh
+++ data.sh
@@ -2,6 +2,6 @@
 sudo yum -y update
 sudo yum -y install git
 git clone https://github.com/Sagar0546/agri.git
-cd Medical-Insurance-py
+cd agri
 pip3 install -r requirements.txt
 screen -m -d python3 app.py
