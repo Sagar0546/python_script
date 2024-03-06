09f97233ed3077ac5aac4561e9117ddad303608a
sagar <150423510+Sagar0546@users.noreply.github.com>
2024-03-06 14:22:27 +0530

    Update data.sh

--- data.sh
+++ data.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 sudo yum -y update
 sudo yum -y install git
-git clone https://github.com/Sagar0546/agri.git
-cd agri
+git clone https://github.com/Sagar0546/fish-py.git
+cd fish
 pip3 install -r requirements.txt
 screen -m -d python3 app.py
